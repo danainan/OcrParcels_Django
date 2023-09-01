c84139ce0ea3d3db864e2f5643223d23b12c8019
danainan <71175110+danainan@users.noreply.github.com>
2023-09-01 13:33:10 +0700

    Update settings.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "django-insecure-@lny6gwv83(&%1=ekfa)=e7)b3!_&t1rkgggpk=#gysr2cwasg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ocrparcelsdjango-production.up.railway.app','','*']
 
 
 
