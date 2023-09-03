ae46040f512809d311e1bff934978d8d0026411c
danainan <71175110+danainan@users.noreply.github.com>
2023-09-03 20:07:14 +0700

    Update settings.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -147,7 +147,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
 
 CSRF_COOKIE_HTTPONLY = False
 
@@ -159,6 +159,7 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+LOGIN_URL = "/login/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "ocr/static/"),
